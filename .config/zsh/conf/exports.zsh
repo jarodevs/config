442b6f72259bc7471dc3c2452f219211b6b0d681
Aria Groult <aria@geteimy.com>
2022-11-14 11:35:42 +0100

    feat(sway): set sway as default desktop

--- .config/zsh/conf/exports.zsh
+++ .config/zsh/conf/exports.zsh
@@ -1,4 +1,5 @@
 export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CURRENT_DESKTOP=sway
 export PATH="$HOME/.local/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
