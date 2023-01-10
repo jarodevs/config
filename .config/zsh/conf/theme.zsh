8c879b08a09699cc946b64c71b91000702d3c482
Aria Groult <aria@geteimy.com>
2023-01-10 15:27:47 +0100

    refactor(zsh): change prompt color to orange

--- .config/zsh/conf/theme.zsh
+++ .config/zsh/conf/theme.zsh
@@ -15,4 +15,4 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#575757"
 #Prompt
 source $ZDOTDIR/scripts/mod-git-prompt.sh
 
-setopt PROMPT_SUBST ; PS1='%B%F{201}%n%f%b in %B%F{117}%2~%f$(mod_git_prompt) '
+setopt PROMPT_SUBST ; PS1='%B%F{208}%n%f%b in %B%F{117}%2~%f$(mod_git_prompt) '
