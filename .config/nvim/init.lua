7ddb85d89cf5aaac776ddd94fac30f92b0d9a8d4
JaroDevs <aria.groult@Pm.me>
2023-10-06 08:30:42 +0100

    refactor(nvim): telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,22 @@
-vim.opt.rnu				= true
-vim.opt.cursorline 		= true
+vim.opt.tabstop 	= 4
+vim.opt.shiftwidth 	= 4
+vim.opt.rnu		= true
+vim.opt.cursorline = true
+vim.g.loaded = 1
+vim.g.loaded_netrwPlugin = 1
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('plugins')
