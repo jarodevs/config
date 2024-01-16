a0725d7b04dfb2549b2510565d4bd53d573a535e
JaroDevs <aria.groult@Pm.me>
2024-01-16 11:37:54 +0100

    refactor: put org before babel load

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184" default))
+   '("c171012778b7cf795ac215b91e1ecab8e3946738d03095397a790ed41e0a3386" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184" default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
    '(impatient-mode simple-httpd ace-window dashboard page-break-lines page-break-line spacious-padding golden-ratio solaire-mode modus-themes org-agenda drag-stuff org-wild-notifier terminal-notifier gruber-darker-theme flycheck-rust rustic autothemer orderless cape corfu lsp-ui lsp-mode multi-vterm vterm ayu-theme doom-themes cyberpunk-theme eterm-256color forge yasnippet terraform-mode hcl-mode flycheck neotree emacs-neotree org-mode ivy-prescient exec-path-from-shell evil-magit magit counsel-projectile projectile general all-the-icons helpful ivy-rich counsel swiper ivy which-key doom-modeline evil))
@@ -31,4 +31,33 @@
  '(window-divider-last-pixel ((t :background "#000000" :foreground "#000000"))))
 
 (setq package-enable-at-startup nil)
+
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name
+	  "straight/repos/straight.el/bootstrap.el"
+	  (or (bound-and-true-p straight-base-dir)
+	      user-emacs-directory)))
+	(bootstrap-version 7))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+  (straight-use-package 'use-package)
+  (setq straight-use-package-by-default t)
+
+(use-package org
+  :hook
+    (org-mode . org-indent-mode)
+  :config
+    (setq org-agenda-files
+	  '("~/Documents/org/")
+	  org-ellipsis " ▾"
+	  org-log-done 'time)
+    (evil-set-initial-state 'org-agenda-mode 'normal))
+
 (org-babel-load-file "~/.config/emacs/config.org")
