122079700eeec4b2022dc88affccf89033abeedb
JaroDevs <aria.groult@Pm.me>
2024-02-26 15:25:50 +0100

    refactor: delete to everything minimal

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,10 +4,27 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c171012778b7cf795ac215b91e1ecab8e3946738d03095397a790ed41e0a3386" "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db" "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184" default))
+   '("e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
+     "c171012778b7cf795ac215b91e1ecab8e3946738d03095397a790ed41e0a3386"
+     "21055a064d6d673f666baaed35a69519841134829982cbbb76960575f43424db"
+     "15604b083d03519b0c2ed7b32da6d7b2dc2f6630bef62608def60cdcf9216184"
+     default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(impatient-mode simple-httpd ace-window dashboard page-break-lines page-break-line spacious-padding golden-ratio solaire-mode modus-themes org-agenda drag-stuff org-wild-notifier terminal-notifier gruber-darker-theme flycheck-rust rustic autothemer orderless cape corfu lsp-ui lsp-mode multi-vterm vterm ayu-theme doom-themes cyberpunk-theme eterm-256color forge yasnippet terraform-mode hcl-mode flycheck neotree emacs-neotree org-mode ivy-prescient exec-path-from-shell evil-magit magit counsel-projectile projectile general all-the-icons helpful ivy-rich counsel swiper ivy which-key doom-modeline evil))
+   '(impatient-mode simple-httpd ace-window dashboard page-break-lines
+		    page-break-line spacious-padding golden-ratio
+		    solaire-mode modus-themes org-agenda drag-stuff
+		    org-wild-notifier terminal-notifier
+		    gruber-darker-theme flycheck-rust rustic
+		    autothemer orderless cape corfu lsp-ui lsp-mode
+		    multi-vterm vterm ayu-theme doom-themes
+		    cyberpunk-theme eterm-256color forge yasnippet
+		    terraform-mode hcl-mode flycheck neotree
+		    emacs-neotree org-mode ivy-prescient
+		    exec-path-from-shell evil-magit magit
+		    counsel-projectile projectile general
+		    all-the-icons helpful ivy-rich counsel swiper ivy
+		    which-key doom-modeline evil))
  '(safe-local-variable-values '((lsp-eslint-options "--config" "./functions/.eslintrc")))
  '(warning-suppress-log-types '((comp))))
 (custom-set-faces
